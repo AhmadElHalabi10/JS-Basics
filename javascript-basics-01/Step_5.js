1497af29b6e2898f7be8850e23118f37e48b817d
AhmadElHalabi10 <ahmd.el.halabi.4@gmail.com>
2022-12-09 09:47:31 +0200

    Step 5 J-B-1

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+document.getElementById("validate").onclick = function () {
+  var X = document.getElementById("first_number").value;
+  var Y = document.getElementById("second_number").value;
+
+  alert(X * Y);
+};
